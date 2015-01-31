ad4ec3b0320519acd4d86de5276cb5e19dd2bee0
thesafa <safa.eristi@gmail.com>
2015-01-31 19:04:02 +0300

    Bootstrap Plugin Added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,12 +60,13 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:1.9.9"
+		compile ":asset-pipeline:2.1.1" // Added By Safa
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+		runtime ':twitter-bootstrap:3.3.1' // Added By Safa
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
